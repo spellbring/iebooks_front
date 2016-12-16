6bad92132d2449e08868029550e2470203415bd7
spellbring <jjreyes.romero88@gmail.com>
2016-12-16 14:41:09 -0300

    Se modifica el cuento para que se visualice en la presentación

--- application/views/back_end/cuento.php
+++ application/views/back_end/cuento.php
@@ -3,7 +3,7 @@
 
 
        
-            <iframe src="assets/resources/media/anim_interactiva/Cerdito/Cerdito.html" width="100%" height="500px">
+            <iframe src="assets/resources/media/animaciones/1.html" width="100%" height="500px">
 
              </iframe>
        
